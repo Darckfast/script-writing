50dd44fb82bb7bc81d8f77d64e02f49f1c7cdd5c
Victor Costa <victorcosta37@gmail.com>
2022-12-09 17:15:04 -0300

    chore: clean up

--- src/components/Button.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let onClick: () => void
-</script>
-
-<button
-	class="bg-indigo-700  px-4 py-2 rounded transition-all hover:bg-indigo-600"
-	on:click={() => onClick()}><slot /></button
->
