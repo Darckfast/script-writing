9adb5509213a88c8c3142574ba6874ac071289b3
Victor Costa <victorcosta37@gmail.com>
2023-01-05 10:24:48 -0300

    feat: add icons

---
+++ src/styles/icons/minimize.svelte
@@ -0,0 +1,8 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	fill="currentColor"
+	preserveAspectRatio="xMidYMid meet"
+	viewBox="0 0 24 24"><path fill="currentColor" d="M19 13H5v-2h14v2Z" /></svg
+>
