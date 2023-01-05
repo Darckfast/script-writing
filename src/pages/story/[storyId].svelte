13a82f8ef21c52e08aa9d17d9f9f671488461003
Victor Costa <victorcosta37@gmail.com>
2023-01-05 10:24:28 -0300

    refactor: redone the page

--- src/pages/story/[storyId].svelte
+++ src/pages/story/[storyId].svelte
@@ -1,150 +1,69 @@
 <script lang="ts">
-	import NodeCard from '../../components/NodeCard.svelte'
-	import PropInput from '../../components/PropInput.svelte'
-	import { onDestroy } from 'svelte'
-	import { v4 as uuidv4 } from 'uuid'
-	import { load, save } from '../../lib/loadSave'
-	import { add, props, remove, update } from '../../lib/nodesv2'
-	import { stories } from '../../lib/stories'
 	import { url } from '@roxi/routify'
+	import { writeText } from '@tauri-apps/api/clipboard'
+	import { v4 as uuidv4 } from 'uuid'
+	import NodeCard from '../../components/story/NodeCard.svelte'
+	import ConfigsMenu from '../../components/story/props/ConfigsMenu.svelte'
+	import PropsMenu from '../../components/story/props/PropsMenu.svelte'
+	import { add, remove } from '../../lib/nodesv2'
+	import { getStory } from '../../lib/stores/story'
 	import ArrowLeft from '../../styles/icons/arrow-left.svelte'
 	import Copy from '../../styles/icons/copy.svelte'
+	import Gear from '../../styles/icons/gear.svelte'
+	import Magnet from '../../styles/icons/magnet.svelte'
 
 	export let storyId: string
 
-	let storyProps = []
-	let localProps = load<Props[]>(`props-${storyId}`)
-	let propName = ''
-	let propValue = null
-	let story: Story
+	let selectedIndex = 0
+	let tabOpen = 'props'
+	const story = getStory(storyId)
 
-	let selectedPid = ''
-	let propType = 'text'
+	$: selectedNode = $story.passages[selectedIndex]
 
-	const propTypes = ['text', 'boolean', 'number']
-
-	$: selectedNode = passages.find((p) => p.pid === selectedPid) ?? {
-		cleanText: '',
-		pid: uuidv4()
-	}
-	$: passages = story?.passages ?? []
-	$: nodeProps = props(selectedNode)
-	$: {
-		stories.update((values: Story[]) =>
-			values.map((value) => {
-				if (value.ifid === story.ifid) {
-					return {
-						...story,
-						passages
-					}
-				}
-
-				return value
-			})
-		)
-
-		localProps.sort((a, b) => a.name.localeCompare(b.name))
-		save(`props-${storyId}`, localProps)
-	}
-
-	const unSubStory = stories.subscribe((value: Story[]) => {
-		story = value.find((v) => v.ifid === storyId)
-
-		if (!story) return
-
-		if (!selectedPid && story.passages.length) {
-			selectedPid = story.passages[story.passages.length - 1].pid
-		}
-
-		storyProps = props(story)
-	})
-
-	const createProp = () => {
-		localProps = [
-			...localProps,
-			{ name: propName, value: propValue, type: propType }
-		]
-
-		propName = ''
-		propValue = ''
-	}
-
-	const removePropFromLocal = (index: number) => {
-		localProps.splice(index, 1)
-
-		localProps = [...localProps]
-	}
-
-	const removePropFromStory = (name: string) => {
-		delete story[name]
-
-		story = {
-			...story
-		}
-	}
-
-	const removePropFromNode = (name: string) => {
-		delete selectedNode[name]
-
-		passages = [...update({ nodes: story.passages, update: selectedNode })]
-	}
-
-	const addNode = (copyOfNode = {}) => {
-		const newNodePid = uuidv4()
-		passages = [
+	const addNode = (node = {}) => {
+		$story.passages = [
 			...add({
-				nodes: passages,
+				nodes: $story.passages,
 				add: {
-					...copyOfNode,
-					parentPid: selectedPid ?? uuidv4(),
-					pid: newNodePid,
-					name: passages.length + 1,
+					...node,
+					parentPid: selectedNode.pid ?? uuidv4(),
+					pid: uuidv4(),
+					name: $story.passages.length + 1,
 					cleanText: '',
 					links: []
 				}
 			})
 		]
 
-		selectedPid = newNodePid
+		selectedIndex = $story.passages.length - 1
 	}
 
 	const deleteNode = (node: StoryNode) => {
-		passages = [...remove({ nodes: passages, remove: node })]
-	}
+		if (selectedIndex === 0) return
 
-	const updateNode = (event: CustomEvent<Props>) => {
-		const { name, value } = event.detail
+		const newIndex = $story.passages.findIndex(
+			({ pid }) => pid === selectedNode.pid
+		)
 
-		passages = [
-			...update({
-				nodes: story.passages,
-				update: {
-					pid: selectedPid,
-					[name]: value
-				}
-			})
-		]
-	}
+		if (newIndex !== -1) {
+			selectedIndex = newIndex
+		} else {
+			selectedIndex = $story.passages.findIndex(
+				({ pid }) => pid === selectedNode.parentPid
+			)
+		}
 
-	const setProp = (event: CustomEvent<Props>) => {
-		const { name, value } = event.detail
+		if (node.pid === selectedNode.pid) {
+			selectedIndex = $story.passages.findIndex(
+				({ pid }) => pid === selectedNode.parentPid
+			)
+		}
 
-		passages = [
-			...passages.map((passage) => {
-				if (passage.pid === selectedPid) {
-					return {
-						...passage,
-						[name]: value
-					}
-				}
-
-				return passage
-			})
-		]
+		$story.passages = [...remove({ nodes: $story.passages, remove: node })]
 	}
 
 	const getAsArray = () =>
-		passages.map((passage) => {
+		$story.passages.map((passage) => {
 			delete passage.links
 			delete passage.parentPid
 
@@ -161,152 +80,87 @@
 
 			return passage
 		})
-
-	onDestroy(() => unSubStory())
 </script>
 
-<header class="flex justify-between items-center w-full gap-2 px-2">
-	<a href={$url('..')} class="btn btn-primary"> <ArrowLeft /> go back</a>
-	<h1>{story?.storyName}</h1>
+<form on:submit|preventDefault class="w-full h-full">
+	<header class="flex justify-between items-center w-full gap-2 px-2">
+		<a href={$url('../../..')} class="btn btn-primary">
+			<ArrowLeft /> go back</a
+		>
+		<h1>{$story.storyName}</h1>
 
-	<button
-		class="btn btn-xs gap-2"
-		on:click={() => navigator.clipboard.writeText(storyId)}
-	>
-		<span class="text-xs">{storyId}</span><Copy /></button
-	>
+		<button
+			class="btn btn-xs gap-2 w-auto text-sm overflow-auto overflow-ellipsis"
+			on:click={() => writeText(storyId)}
+		>
+			{storyId}<Copy /></button
+		>
 
-	<button
-		class="btn btn-primary"
-		on:click={() => navigator.clipboard.writeText(JSON.stringify(story))}
-		>copy story</button
-	>
-	<button
-		class="btn btn-primary"
-		on:click={() => navigator.clipboard.writeText(JSON.stringify(getAsArray()))}
-		>copy as array</button
-	>
-</header>
-<div class="flex items-center justify-center w-full h-4/5 gap-2 p-2">
-	<div
-		class="flex items-center justify-center relative flex-wrap gap-2 p-4 w-1/2 h-auto max-h-full overflow-y-scroll custom-scroll "
-	>
-		{#each passages as node}
-			<NodeCard
-				{...node}
-				isSelected={node?.pid === selectedPid}
-				on:select={(e) => (selectedPid = e.detail.pid)}
-				on:remove={() => deleteNode(node)}
-			/>
-		{/each}
-
-		<div class="mt-2">
-			<button class="btn btn-primary" on:click={addNode}>+ new</button>
-			<button class="btn btn-primary" on:click={() => addNode(selectedNode)}
-				>+ duplicate node</button
-			>
-		</div>
-	</div>
-
-	<div class="flex items-center justify-center flex-col w-1/2 h-full">
+		<button
+			class="btn btn-primary w-auto "
+			on:click={() => writeText(JSON.stringify(story))}>> copy</button
+		>
+		<button
+			class="btn btn-primary"
+			on:click={() => writeText(JSON.stringify(getAsArray()))}>> array</button
+		>
+	</header>
+	<div class="flex items-center justify-center w-full h-4/5 gap-2 p-2">
 		<div
-			class="flex justify-between items-center p-1 gap-2 flex-wrap overflow-y-scroll custom-scroll"
+			class="flex items-center justify-center relative flex-wrap gap-2 p-4 w-1/2 h-auto max-h-full overflow-y-scroll custom-scroll "
 		>
-			{#each storyProps as prop}
-				<PropInput
-					{...prop}
-					on:change={setProp}
-					on:remove={() => removePropFromStory(prop.name)}
+			{#each $story.passages as node, index}
+				<NodeCard
+					cleanText={node.cleanText}
+					image={node.image}
+					name={node.name}
+					sentBy={node.sentBy}
+					isSelected={index === selectedIndex}
+					on:select={() => (selectedIndex = index)}
+					on:remove={() => deleteNode(node)}
 				/>
 			{/each}
 
-			{#each nodeProps as prop}
-				<PropInput
-					{...prop}
-					on:change={setProp}
-					on:remove={() => removePropFromNode(prop.name)}
-				/>
-			{/each}
-
-			<hr class="m-4 w-full border-t-2 border-primary" />
-
-			{#each localProps as prop, index}
-				<PropInput
-					{...prop}
-					on:change={updateNode}
-					on:remove={() => removePropFromLocal(index)}
-					on:add={updateNode}
-					isAddable
-				/>
-			{/each}
+			<div class="mt-2">
+				<button class="btn btn-primary" on:click={addNode}>+ new</button>
+				<button class="btn btn-primary" on:click={() => addNode(selectedNode)}
+					>+ duplicate node</button
+				>
+			</div>
 		</div>
 
-		<form
-			on:submit|preventDefault
-			class="mt-2 flex justify-center items-center flex-wrap ring-1 ring-primary p-2 rounded "
-		>
-			<div class="w-full flex justify-between items-center">
-				<label>
-					<input
-						placeholder="name"
-						class="input input-primary input-sm"
-						bind:value={propName}
-					/>
-					<span>:</span>
-				</label>
-
-				<label class="p-1">
-					{#if propType === 'boolean'}
-						<input
-							type="checkbox"
-							bind:checked={propValue}
-							class="toggle toggle-primary"
-						/>
-					{:else if propType === 'number'}
-						<input
-							placeholder="0"
-							type="number"
-							bind:value={propValue}
-							class="input input-primary input-sm"
-						/>
-					{:else}
-						<input
-							placeholder="value"
-							bind:value={propValue}
-							class="input input-primary input-sm"
-						/>
-					{/if}
-				</label>
-
-				<button class="btn btn-primary" on:click={createProp}>add</button>
+		<div class="flex items-center justify-center flex-wrap w-1/2 h-full">
+			<div class="tabs tabs-boxed w-full self-start">
+				<button
+					on:click={() => (tabOpen = 'props')}
+					class="tab gap-2"
+					class:tab-active={tabOpen === 'props'}><Magnet /> Props</button
+				>
+				<button
+					on:click={() => (tabOpen = 'configs')}
+					class="tab gap-2"
+					class:tab-active={tabOpen === 'configs'}
+				>
+					<Gear /> Config</button
+				>
 			</div>
-
-			<div class="w-full flex gap-4">
-				{#each propTypes as type}
-					<label class="flex items-center gap-2">
-						<input
-							bind:group={propType}
-							value={type}
-							type="radio"
-							name="radio-2"
-							class="radio radio-primary"
-						/>
-						{type}
-					</label>
-				{/each}
-			</div>
-		</form>
+			{#if tabOpen === 'props'}
+				<PropsMenu bind:node={$story.passages[selectedIndex]} />
+			{:else if tabOpen === 'configs'}
+				<ConfigsMenu {storyId} />
+			{/if}
+		</div>
 	</div>
-</div>
 
-<form on:submit|preventDefault class="flex self-end w-full h-auto p-2 gap-1">
-	<label class="w-full text-sm">
-		<input
-			bind:value={selectedNode.cleanText}
-			class="
+	<div class="flex self-end w-full h-auto p-2 gap-1">
+		<label class="w-full text-sm">
+			<input
+				bind:value={$story.passages[selectedIndex].cleanText}
+				class="
       h-20 
       w-full 
       input input-primary input-lg"
-		/>
-	</label>
+			/>
+		</label>
+	</div>
 </form>
