6a5c2c852bb874cd992c9564bdf580688fdb9426
Victor Costa <victorcosta37@gmail.com>
2023-01-05 10:17:45 -0300

    feat: add title bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
 	import { Router } from '@roxi/routify'
 	import { routes } from '../.routify/routes'
+	import TitleBar from './components/titleBar/TitleBar.svelte'
 </script>
+
+<TitleBar />
 
 <Router {routes} />
