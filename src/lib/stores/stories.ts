f98747c388d11d45c78e2638e007bbd55041bd41
Victor Costa <victorcosta37@gmail.com>
2023-01-05 10:22:49 -0300

    feat: add checkForPromises

---
+++ src/lib/stores/stories.ts
@@ -0,0 +1,63 @@
+import { writable } from 'svelte/store'
+import { createLocalDataFolder, loadV2, saveV2 } from '../loadSave'
+
+const createStories = () => {
+	createLocalDataFolder()
+
+	const { set, subscribe, update } = writable<Story[]>([])
+
+	const checkForPromises = (value: any, storyId?: string) => {
+		if (value instanceof Array) {
+			value.map((subElem) => checkForPromises(subElem, subElem.ifid))
+		}
+
+		for (const key in value) {
+			if (Object.prototype.hasOwnProperty.call(value, key)) {
+				const element = value[key]
+
+				if (element instanceof Promise) {
+					element.then((res) => {
+						update((currStories) =>
+							currStories.map((story) => {
+								if (story.ifid === storyId) {
+									story.passages = story.passages.map((passage) => {
+										if (passage.pid === value.pid) {
+											passage[key] = res
+										}
+
+										return passage
+									})
+								}
+
+								return story
+							})
+						)
+					})
+				}
+
+				if (element instanceof Array) {
+					element.map((subElem) => checkForPromises(subElem, storyId))
+				}
+			}
+		}
+	}
+
+	loadV2<Story[]>({ key: 'stories' })
+		.then((res) => set(res))
+		.then(() => {
+			subscribe((value) => {
+				if (!value.length) return
+
+				checkForPromises(value)
+
+				saveV2({ key: 'stories', value })
+			})
+		})
+
+	return {
+		set,
+		subscribe,
+		update
+	}
+}
+export const stories = createStories()
