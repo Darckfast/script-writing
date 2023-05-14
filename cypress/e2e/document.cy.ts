c982a8610cb1cfc934140b7829909dea06008be9
Victor Costa <victorcosta37@gmail.com>
2023-05-14 09:50:21 -0300

    test: updated tests

--- cypress/e2e/document.cy.ts
+++ cypress/e2e/document.cy.ts
@@ -1,17 +1,32 @@
 describe('document page', () => {
+  const getCopyBtn = () =>  cy.get('button[data-test="btn-copy-content"]')
+  const getDeleteBtn = () =>  cy.get('button[data-test="btn-delete-document"]')
+  const createDoc = () => 		cy.get('button[data-test="btn-create-document"]').click()
+  const accessDoc = () => cy.get('a[data-test="a-document-node-0"]').click()
+
 	beforeEach(() => {
 		cy.visit('http://localhost:5177')
 		cy.get('input').type('new document')
-		cy.get('button[data-test="btn-create-document"]').click()
-		cy.get('a[data-test="a-document-node-0"]').click()
 	})
 
 	it('create document content', () => {
+    createDoc() 
+		accessDoc()
+
 		cy.get('div.cm-content').clear().type('{"test": true}',
      { 
       force: true,
       parseSpecialCharSequences:false 
     })
+
+    getCopyBtn().click()
+	})
+
+  it('create and remove document', () => {
+    createDoc()
+    getDeleteBtn().dblclick()
+
+    cy.get('span[data-test="empty-doc-placeholder"]').should('have.text', 'no documents available')
 	})
 
 })
