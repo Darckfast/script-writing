1926b85d3b9ceeaa74101ef382d705309b2acdd6
Victor Costa <victorcosta37@gmail.com>
2023-06-04 16:42:20 -0300

    feat: add node number input

---
+++ src/components/input/NumberInput.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { CustomWritable } from 'svelvet'
+
+	export let isAddable = false
+	export let propName = ''
+	export let parameterStore: CustomWritable<any>
+
+</script>
+
+<input
+	data-test={`input-${isAddable ? 'local' : 'node'}-prop-${propName}`}
+	type="number"
+	step="any"
+	bind:value={$parameterStore}
+	class="input w-full input-primary input-sm"
+/>
