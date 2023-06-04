2be4b5c11fb66ba7f42df3cbf18ce2e440b7f291
Victor Costa <victorcosta37@gmail.com>
2023-06-04 16:41:26 -0300

    feat: add float header option

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -1,43 +1,75 @@
 <script lang="ts">
-	import { url } from '@roxi/routify'
+	import { goto } from '@roxi/routify'
+	import { appWindow } from '@tauri-apps/api/window'
 	import type { Readable } from 'svelte/store'
+	import { copy } from '../../lib/copy'
 	import { isDbxAuth } from '../../lib/stores/dbx'
 	import ArrowLeft from '../../styles/icons/arrow-left.svelte'
 	import Spinner from '../../styles/icons/spinner.svelte'
 
 	export let headerName: string
-	export let onCopy: () => void
-	export let onSync: () => void
-	export let isFetching: Readable<boolean>
+	export let onCopy: () => void = undefined
+	export let onSync: () => void = undefined
+	export let isFetching: Readable<boolean> = undefined
 	export let id: string
+	export let floatHeader = false
+	export let onReturn: () => void = undefined
+
+	const goBack = () => {
+		if (onReturn) onReturn()
+
+		if (history.length > 1) {
+			$goto('../../..')
+			return
+		}
+
+		appWindow.close()
+	}
 </script>
 
-<header class="flex justify-between items-center w-full gap-2 px-2 pb-4">
-	<a href={$url('../../..')} data-test="btn-return" class="btn btn-primary">
-		<ArrowLeft /> go back</a
+<header
+	class:floatHeader
+	class="flex justify-between items-center w-full gap-2 px-2 pb-4"
+>
+	<button on:click={goBack} data-test="btn-return" class="btn btn-primary">
+		<ArrowLeft /> go back</button
 	>
 	<h1 data-test="story-name">{headerName}</h1>
 
 	<button
+		on:click={() => copy(id)}
 		class="btn btn-xs gap-2 w-auto text-sm overflow-auto overflow-ellipsis"
 	>
 		{id}</button
 	>
 
-	<button
-		class="btn btn-primary w-auto"
-		data-test="btn-copy-content"
-		on:click={onCopy}>> copy</button
-	>
+	{#if onCopy}
+		<button
+			class="btn btn-primary w-auto"
+			data-test="btn-copy-content"
+			on:click={onCopy}>> copy</button
+		>
+	{/if}
 
-	<button
-		class="btn btn-primary w-auto"
-		disabled={!isDbxAuth()}
-		on:click={onSync}
-		>{#if $isFetching}
-			<Spinner />saving...
-		{:else}
-			> save
-		{/if}</button
-	>
+	{#if onSync}
+		<button
+			class="btn btn-primary w-auto"
+			disabled={!isDbxAuth()}
+			on:click={onSync}
+			>{#if $isFetching}
+				<Spinner />saving...
+			{:else}
+				> save
+			{/if}</button
+		>
+	{/if}
 </header>
+
+<style>
+	.floatHeader {
+		width: 90%;
+		padding: 0;
+		position: absolute;
+		z-index: 10;
+	}
+</style>
