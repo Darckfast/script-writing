898e0aa0c6137e9bc0c5cf077f557485fa042837
Victor Costa <victorcosta37@gmail.com>
2023-06-14 21:08:13 -0300

    feat: add show/hide button

---
+++ src/components/button/ShowHideButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import EyeSlashSolid from '../../styles/icons/eye-slash-solid.svelte'
+	import EyeSolid from '../../styles/icons/eye-solid.svelte'
+
+	export let show = true
+</script>
+
+<button
+	on:click={() => (show = !show)}
+	class={`transition-all hover:scale-105 ${$$props.class}`}
+>
+	{#if show}
+		<EyeSlashSolid />
+	{:else}
+		<EyeSolid />
+	{/if}
+</button>
